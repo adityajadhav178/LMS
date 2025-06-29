b0af2e5ebb1f6f2fa1814f1d3a07ee457df9bf16
Aditya <adityacj178@gmail.com>
2025-06-29 12:49:59 +0530

    Correction 1

--- Server/Controllers/webhooks.js
+++ Server/Controllers/webhooks.js
@@ -15,7 +15,7 @@ export const clerkWebhooks = async (req ,res) => {
                 const userData = {
                     _id: data.id,
                     mame: data.first_name + " " + data.last_name,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     imageUrl: data.image_url
                 }
                 await User.create(userData);
@@ -25,7 +25,7 @@ export const clerkWebhooks = async (req ,res) => {
             
             case 'user.updated': {
                 const userData = {
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url
                 }
